80f4c285f3a4e12a009edb95cd74527c39cb01e8
Javier <javierohern@me.com>
2023-10-02 21:50:23 -0700

    Feature 6 complete

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-let secretNumber = Math.floor(Math.random() * 100) +1;
+let playAgain;
+do {
+  let secretNumber = Math.floor(Math.random() * 100) + 1;
+  let name = String(prompt("What's your name?"));
+  let guess = Number(prompt("Guess a number between 1 and 100"));
+  let tries = [];
 
-let name = String(prompt("What's your name?"))
-let guess = Number(prompt("Guess a number between 1 and 100"))
-
-let tries = [];
-
-// if (guess === secretNumber) {
-//     alert("That's correct!");
-// } else if (guess < secretNumber) {
-//     alert("Higher!")
-// } else {
-//     alert("Lower")
-// }
-
-while (guess !== secretNumber) {
-    tries.push(guess)
+  while (guess !== secretNumber) {
+    tries.push(guess);
     if (guess < secretNumber) {
-        guess = Number(prompt(`Sorry ${name}, guess HIGHER!`))
+      guess = Number(prompt(`Sorry ${name}, guess HIGHER!`));
+    } else if (guess > secretNumber) {
+      guess = Number(prompt(`Sorry ${name}, guess LOWER!`));
     }
-    else if (guess > secretNumber) {
-        guess = Number(prompt(`Sorry ${name}, guess LOWER!`))
-    }
-}
-alert(`That's correct ${name}! Your previous guesses were ` + tries.join(", "))
-console.log(tries)
+  }
+  alert(
+    `That's correct ${name}! Your previous guesses were ` + tries.join(", ")
+  );
+  playAgain = String(prompt("Would you like to play again?"))
+} while (playAgain === "yes");
