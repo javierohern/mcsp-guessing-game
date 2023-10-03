08e513293561687505180f98e1983fb820ef357c
Javier <javierohern@me.com>
2023-10-02 21:30:45 -0700

    Feature 3 complete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let secretNumber = Math.floor(Math.random() * 100) +1;
 
 let guess = Number(prompt("Guess a number between 1 and 100"))
 
+let tries = 0
+
 if (guess === secretNumber) {
     alert("That's correct!");
 } else if (guess < secretNumber) {
@@ -12,10 +14,12 @@ if (guess === secretNumber) {
 
 while (guess !== secretNumber) {
     if (guess < secretNumber) {
+        tries++
         guess = Number(prompt("Try again, this time guess a HIGHER number!"))
     }
     else if (guess > secretNumber) {
+        tries++
         guess = Number(prompt("Try again, this time guess a LOWER number!"))
     }
 }
-alert("correct!")
+alert(`That's correct! It took you ${tries} tries!`)
